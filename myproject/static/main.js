f8346a3e946ced4c855b66528eea7aa474e91337
feudal <calmis87@gmail.com>
2021-05-09 20:04:51 +0300

    activate links in menu

--- myproject/static/main.js
+++ myproject/static/main.js
@@ -10,6 +10,24 @@ function toggle(link) {
 tog_menu = document.getElementById("toggle_menu")
 tog_menu.addEventListener("click", toggle_menu)
 
+
 function toggle_menu() {
 	document.getElementById("id_menu").classList.toggle('active');
+}
+
+// ------------------active links---------------------------
+var pathname = window.location.pathname; 
+console.log(pathname)
+
+var btnContainer = document.getElementById("id_menu");
+var btns = btnContainer.getElementsByClassName("link");
+
+// if (pathname == '/') {
+// 	btns[0].classList.add('active')
+// }
+if (pathname == '/players') {
+	btns[1].classList.add('active')
+}
+if (pathname == '/other_players') {
+	btns[2].classList.add('active')
 }
